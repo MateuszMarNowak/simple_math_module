7aef569fc2e46cc6a6bd92d324787f189d9d2168
MateuszMarNow <mateusznow94@gmail.com>
2023-03-03 15:08:54 +0100

    adds tests

---
+++ simple_math_module/tests/extras_tests/divide_test.py
@@ -0,0 +1,24 @@
+import unittest
+from ...extras.divide import divide
+
+class TestDivide(unittest.TestCase):
+    
+    def test_divide(self):
+        a, b = 2, 1
+        self.assertEquals(divide(a, b), 2)
+
+    def test_divide_float(self):
+        a, b = 1, 2
+        self.assertEquals(divide(a, b), 0.5)
+
+    def test_divide_negative_arguments(self):
+        a, b = 4, -2
+        self.assertEquals(divide(a, b), -2)
+
+    def test_division_by_zero_raise_exception(self):
+        a, b = 4, 0
+        with self.assertRaises(Exception) as context:
+            divide(a, b)
+        
+        self.assertTrue("Division by zero is not permitted" in str(context.exception))
+        
